7f2e705d19a80a0c0285381fabb4ce2f5b0d684f
Scraapy <rockinmdk@gmail.com>
2015-06-18 16:48:48 -0400

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,9 @@
+package me.Scrap.Limiter;
+
+import org.bukkit.ChatColor;
+
+public class Constants
+{
+  public static final String PLUGIN_NAME = "Stack Limiter";
+  public static final String PLUGIN_PREFIX = ChatColor.DARK_AQUA + "[Stack Limiter]";
+}
